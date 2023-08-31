493e322ac4b238579fd43b515eac8a0aae84ed06
Vinnih-1 <vinifantal@hotmail.com>
2023-08-30 23:28:12 -0300

    feat: implement react-auth-kit before routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { LoginPage } from "./pages/Login"
 import { HomePage } from "./pages/Home"
 import { RegisterPage } from "./pages/Register"
 
+import { AuthProvider } from "react-auth-kit";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={HomePage} />
-        <Route path="/login" Component={LoginPage} />
-        <Route path="/register" Component={RegisterPage} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider authName="_auth" authType="cookie" cookieDomain={window.location.hostname}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={HomePage} />
+            <Route path="/login" Component={LoginPage} />
+            <Route path="/register" Component={RegisterPage} />
+          </Routes>
+        </BrowserRouter>
+    </AuthProvider>
   )
 }
 
