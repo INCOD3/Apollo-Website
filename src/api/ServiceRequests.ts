61e1155fa9ebc2c8e6f81543c12e528edfe28b50
Vinnih-1 <vinifantal@hotmail.com>
2023-09-03 17:19:02 -0300

    feat: request functions

--- src/api/ServiceRequests.ts
+++ src/api/ServiceRequests.ts
@@ -2,29 +2,29 @@ import axios from "axios";
 
 const api = "http://192.168.1.106:8080/dashboard/";
 
-interface ProductProps {
+export interface ProductProps {
     id: number;
     name: string;
     price: number;
     description: string;
+    onDelete?: () => void;
 }
 
-interface CouponProps {
+export interface CouponProps {
     id: number;
     name: string;
     percentage: number;
     description: string;
     expirateAt: string;
+    onDelete?: () => void;
 }
 
 export interface ServiceProps {
     id: string;
-    owner: string;
     discordId?: string;
     pix?: string;
     products?: Array<ProductProps>;
     coupons?: Array<CouponProps>;
-    createAt: string;
     expirateAt?: string;
 }
 
@@ -37,4 +37,58 @@ export async function getServiceFromToken(token: string): Promise<ServiceProps> 
     });
     const service: ServiceProps = response.data;
     return service;
+}
+
+export async function createProduct(token: string, data: ProductProps): Promise<ProductProps> {
+    const response = await axios.post(api + "createProduct", data, {
+        headers: {
+            Authorization: "Bearer " + token
+        }
+    })
+    const product: ProductProps = response.data;
+    return product;
+}
+
+export function deleteProduct(token: string, productId: number, onSuccess: () => void) {
+    axios({ 
+        method: "delete",
+        url: api + "deleteProduct",
+        data: {
+            id: productId
+        },
+        headers: {
+            Authorization: "Bearer " + token
+        }
+     }).then(() => {
+        onSuccess();
+     }).catch(() => {
+        // chamar um toast de erro
+     });
+}
+
+export async function createCoupon(token: string, data: CouponProps): Promise<CouponProps> {
+    const response = await axios.post(api + "createCoupon", data, {
+        headers: {
+            Authorization: "Bearer " + token
+        }
+    });
+    const coupon: CouponProps = response.data;
+    return coupon; 
+}
+
+export function deleteCoupon(token: string, couponId: number, onSuccess: () => void) {
+    axios({
+        method: "delete",
+        url: api + "deleteCoupon",
+        data: {
+            id: couponId
+        },
+        headers: {
+            Authorization: "Bearer " + token
+        }
+    }).then(() => {
+        onSuccess();
+    }).catch((error) => {
+        console.log(error)
+    });
 }
