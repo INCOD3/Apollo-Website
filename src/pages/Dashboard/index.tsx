a81dd8713ab981b3020b8a1f1223d3f1d93d3ce9
Vinnih-1 <vinifantal@hotmail.com>
2023-09-07 22:48:20 -0300

    fix: remove debug

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,7 +35,6 @@ export function DashboardPage() {
                 return prevService = service;
             })
         }).catch(error => {
-            console.log(service)
             console.log(error)
         });
     }, [])
